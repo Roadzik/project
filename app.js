4818059816306a066fc8c8aae32585f68e36d66a
sobo32 <Roadzik66@gmail.com>
2023-12-07 23:57:45 +0100

    Main page changes

--- app.js
+++ app.js
@@ -3,6 +3,28 @@ const express = require('express')
 const mongo = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.7awui74.mongodb.net/?retryWrites=true&w=majority`
 const app = express()
 const mongoose = require('mongoose')
+const fs = require('fs')
+
+function getFiles(dir, files = []) {
+    // Get an array of all files and directories in the passed directory using fs.readdirSync
+    const fileList = fs.readdirSync(dir)
+    // Create the full path of the file/directory by concatenating the passed directory and file/directory name
+    for (const file of fileList) {
+        const name = `${dir}/${file}`
+        // Check if the current file/directory is a directory using fs.statSync
+        if (fs.statSync(name).isDirectory()) {
+        // If it is a directory, recursively call the getFiles function with the directory path and the files array
+        getFiles(name, files)
+        } else {
+        // If it is a file, push the full path to the files array
+        files.push(name)
+        }
+    }
+    return files
+}     
+
+
+
 
 app.listen(5555)
 app.use(express.json())
@@ -11,7 +33,7 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) =>{
-    res.render('index')
+    res.render('index', {data:getFiles('./public/logos')})
 })
 
 app.get('/urlshrt', (req, res) =>{
